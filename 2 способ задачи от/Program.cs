e34b9f4d644775706d9f0dc4f93b9468fe0adb1b
Николай Караков <kolyan777.k9@gmail.com>
2021-06-25 12:23:27 +0300

    Написал метод, цикл

--- 2 способ задачи от/Program.cs
+++ 2 способ задачи от/Program.cs
@@ -4,15 +4,30 @@ namespace _2_способ_задачи_от_
 {
     class Program
     {
-        static void Main(string[] args)
+        private static bool TryGetValue(out int val)
         {
             Console.WriteLine("Введите 5 чисел: ");
-            int x1 = Convert.ToInt32(Console.ReadLine());
+            var t = Console.ReadLine();
+            var r = int.TryParse(t, out val);
+            return r;
+        }
 
-            if (false)
+        static void Main(string[] args)
+        {
+            
+
+            if (!r)
             {
                 Console.WriteLine("Вы ввели не число!");
             }          
+
+
+
+            int x1 = Convert.ToInt32();
+
+
+
+
 
 
             int x2 = Convert.ToInt32(Console.ReadLine());
@@ -24,34 +39,23 @@ namespace _2_способ_задачи_от_
 
             Console.WriteLine($"Запомните эти числа:{x1},{x2},{x3},{x4},{x5}");
 
-            int[] Num = { x1, x2, x3, x4, x5 };
-
+            int[] Nums = { x1, x2, x3, x4, x5 };
             Console.WriteLine("Введите одно число: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            if (number== Num[0])
+            foreach(var num in Nums)
             {
-                Console.WriteLine("Это именно то самое  число!");
+                if (number== num)
+                {
+                    Console.WriteLine("Это именно то самое  число!");
+                }
+                else
+                {
+                    Console.WriteLine("Совсем дурак чтоли, это не оно!");
+                }
             }
-            else if(number==Num[1])
-            {
-                Console.WriteLine("Это именно то самое  число!");
-            }
-            else if (number == Num[2])
-            {
-                Console.WriteLine("Это именно то самое  число!");
-            }
-            else if (number == Num[3])
-            {
-                Console.WriteLine("Это именно то самое  число!");
-            }
-            else if (number == Num[4])
-            {
-                Console.WriteLine("Это именно то самое  число!");
-            }
-            else
-            {
-                Console.WriteLine("Совсем дурак чтоли, это не оно!");
-            }
+
+
+
 
 
                       
