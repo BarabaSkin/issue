f9117d351dfa52c2e8d987b001bf32b2ed2adca9
Николай Караков <kolyan777.k9@gmail.com>
2021-06-28 14:08:20 +0300

    I made a check of the entered values using a loop

--- 2 способ задачи от/Program.cs
+++ 2 способ задачи от/Program.cs
@@ -7,39 +7,55 @@ namespace _2_способ_задачи_от_
         private static bool TryGetValue(out int val)
         {
             var t = Console.ReadLine();     
-            bool r = !Int32.TryParse(t, out val);  
+            bool r = Int32.TryParse(t, out val);  
             return r;
         }
+        
 
 
         static void Main(string[] args)
-        {
-            Console.WriteLine("Введите 5 чисел: "); 
-            TryGetValue(out int x1);
-            TryGetValue(out int x2);
-            TryGetValue(out int x3);
-            TryGetValue(out int x4);
-            TryGetValue(out int x5);
-
-           
-            Console.WriteLine($"Запомните эти числа:{x1},{x2},{x3},{x4},{x5}");
+        { 
 
 
-            int[] Nums = { x1, x2, x3, x4, x5 };
+            Console.WriteLine("Введите 5 чисел: ");
+            var array =new int[5];
 
-            Console.WriteLine("Введите одно число: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            foreach(var num in Nums)
+
+
+            for (var i = 0; i < array.Length; i++)
             {
-                if (number== num)
+                bool isInt = true;
+
+                while (isInt)
                 {
-                    Console.WriteLine("Это оно!");
+
+                    isInt = TryGetValue(out int t);
+                    if (isInt)
+                    {
+                        array[i] = t;
+                        isInt = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели не число!");
+                        isInt = true;
+                    }
+
                 }
-                else
-                {
-                    Console.WriteLine("Это не оно!");
-                }
+
+
             }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
